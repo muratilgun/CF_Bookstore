f47634f34ee7f138594adf9fce51c2bb2da375c3
Ahmet Murat Ilgun <muratilgun34@hotmail.com>
2020-05-01 17:30:00 +0300

    Enable-Migrations

---
+++ CF_Bookstore.DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace CF_Bookstore.DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CF_Bookstore.DataAccess.Context.ProjeContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CF_Bookstore.DataAccess.Context.ProjeContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
